abb20d1de7066b5e799d37d55cbff4249ffb9146
Erpriex <erpriex@gmail.com>
2023-06-03 16:24:02 +0200

    🐛 Disable FALL damages

--- src/main/java/fr/lostaria/spleef/Spleef.java
+++ src/main/java/fr/lostaria/spleef/Spleef.java
@@ -58,6 +58,7 @@ public final class Spleef extends JavaPlugin {
         gameManager.setPhase(SpleefPhase.START);
 
         getServer().getPluginManager().registerEvents(new BlockBreakListener(this), this);
+        getServer().getPluginManager().registerEvents(new EntityDamageListener(), this);
         getServer().getPluginManager().registerEvents(new EntityDamageByEntityListener(), this);
         getServer().getPluginManager().registerEvents(new GameListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerInteractListener(this), this);
---
+++ src/main/java/fr/lostaria/spleef/listeners/EntityDamageListener.java
@@ -0,0 +1,15 @@
+package fr.lostaria.spleef.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class EntityDamageListener implements Listener {
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+        if (event.getCause() == EntityDamageEvent.DamageCause.FALL) {
+            event.setCancelled(true);
+        }
+    }
+}
