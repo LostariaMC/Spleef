4babd6dabac77cb437ed30515f2ae2fa9346a708
Erpriex <erpriex@gmail.com>
2023-06-03 20:55:22 +0200

    🚧 Parameter destruction cooldown

--- src/main/java/fr/lostaria/spleef/Spleef.java
+++ src/main/java/fr/lostaria/spleef/Spleef.java
@@ -7,6 +7,8 @@ import fr.worsewarn.cosmox.API;
 import fr.worsewarn.cosmox.api.achievements.Achievement;
 import fr.worsewarn.cosmox.api.statistics.Statistic;
 import fr.worsewarn.cosmox.game.Game;
+import fr.worsewarn.cosmox.game.configuration.Parameter;
+import fr.worsewarn.cosmox.tools.items.ItemBuilder;
 import fr.worsewarn.cosmox.tools.map.MapLocation;
 import fr.worsewarn.cosmox.tools.map.MapLocationType;
 import fr.worsewarn.cosmox.tools.map.MapTemplate;
@@ -53,6 +55,10 @@ public final class Spleef extends JavaPlugin {
         game.setRestrictedGame();
         game.setPreparationTime(10);
         game.setDefaultFriendlyFire(true);
+
+        Parameter destructionCooldown = new Parameter("destructionCooldown", "", 180f, 20f, 300f, new ItemBuilder(Material.PLAYER_HEAD).customSkull("fcb8f06885d1daafd26cd95b3482cb525d881a67e0d247161b908d93d56d114f").setDisplayName("§fDestruction des plateformes").setLore(Arrays.asList(" ", "§7Durée de cooldown avant la destruction","§7de chaque plateforme"," "," §7Actuellement §e%time")).build(), Arrays.asList(5f, 10f, 20f), false, false);
+        game.addParameter(destructionCooldown);
+
         api.registerNewGame(game);
 
         gameManager.setPhase(SpleefPhase.START);
