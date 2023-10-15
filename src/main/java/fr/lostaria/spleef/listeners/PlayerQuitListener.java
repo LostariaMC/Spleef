8b034d19d3ee71ba0b89a8f1e71de1bfd0cec1fb
Erpriex <erpriex@gmail.com>
2023-10-15 11:51:51 +0200

    🐛 Eliminate on leave

--- src/main/java/fr/lostaria/spleef/listeners/PlayerQuitListener.java
+++ src/main/java/fr/lostaria/spleef/listeners/PlayerQuitListener.java
@@ -2,6 +2,9 @@ package fr.lostaria.spleef.listeners;
 
 import fr.lostaria.spleef.Spleef;
 import fr.worsewarn.cosmox.game.teams.Team;
+import fr.worsewarn.cosmox.tools.chat.Messages;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,6 +23,13 @@ public class PlayerQuitListener implements Listener {
         Player player = event.getPlayer();
         if(!main.getAPI().getTeamUtils().isInTeam(player, Team.SPEC)){
             main.getAPI().getPlayer(player).setTeam(Team.SPEC);
+
+            for(Player pls : Bukkit.getOnlinePlayers()){
+                pls.sendMessage(main.getPrefix() + Messages.BROADCAST_DEATH.formatted(player.getName()) + " §7§o(déconnexion)");
+                pls.playSound(pls.getLocation(), Sound.ENTITY_VILLAGER_DEATH, 0.5f, 0.5f);
+            }
+
+            main.getGameManager().checkWin();
         }
     }
 }
